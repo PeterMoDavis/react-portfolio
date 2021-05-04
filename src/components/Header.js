dfbcc9aa130c3d3a0e7e25491c73db23cb617efb
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-05-04 16:43:57 -0400

    added header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,24 @@
 import React from "react";
 
+const styles = {
+  header: {
+    fontWeight: "400",
+    letterSpacing: "4px",
+    textDecoration: "none",
+    color: "white",
+  },
+};
 const Header = () => {
-  return <div>Hello</div>;
+  return (
+    <div className="d-flex justify-content-between container mt-4">
+      <a style={styles.header} href="#">
+        ABOUT
+      </a>
+      <a style={styles.header} href="#">
+        CONTACT
+      </a>
+    </div>
+  );
 };
 
 export default Header;
