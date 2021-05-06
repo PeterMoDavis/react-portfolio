cf9c3c80f1257e1537685d0bc003a2d9f45e2dcb
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-05-06 10:29:47 -0400

    changed some things

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import Header from "./components/Header";
 import "./app.css";
 import Footer from "./components/Footer";
 import Container from "./components/Container";
-import Welcome from "./components/Welcome";
 import About from "./components/About";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
+          <Switch />
           <Header />
-
           <Switch>
             <Route path="/" exact component={Container} />
+            <Switch />
             <Route path="/about" component={About} />
           </Switch>
           <Footer />
