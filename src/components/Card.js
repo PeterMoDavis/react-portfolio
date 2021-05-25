0aef6977b7644ef283f6a9319f2d7ba0b79183b7
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-05-25 14:41:51 -0400

    adding overlay

--- src/components/Card.js
+++ src/components/Card.js
@@ -24,6 +24,7 @@ const Card = (props) => {
           alt={props.alt}
         />
       </a>
+      <div>Hello</div>
       <div>
         <small style={styles.small}>
           {props.description}{" "}
